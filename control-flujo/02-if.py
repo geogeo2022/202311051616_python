f31fd3391e3f6fc4f0e942e005cb9808136bd9eb
geogeo2022 <97978302+geogeo2022@users.noreply.github.com>
2023-11-09 22:53:16 -0500

    202311092252

---
+++ control-flujo/02-if.py
@@ -0,0 +1,19 @@
+edad=15
+if edad>54:
+    print("Puede ver la pelicula con descuento")
+elif edad>17:
+    print("Puedes ver la pelicula")
+else:
+    print("No puedes entrar")
+
+print("Listo 1")
+
+edad=70
+if edad>65:
+    print("Puede ver la pelicula con super descuento")
+elif edad>54:
+    print("Puedes ver la pelicula con descuento")
+#else:
+#    print("No puedes entrar")
+
+print("Listo 2")
