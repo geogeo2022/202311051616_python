60bfaf78577bf402dcf2ea8a39efa9153dbd9bfb
geogeo2022 <97978302+geogeo2022@users.noreply.github.com>
2023-11-15 21:40:27 -0500

    202311152139

--- calculadora2.py
+++ calculadora2.py
@@ -1,3 +1,5 @@
+import sys
+
 print("Bienvenidos a la calculadora 2.0")
 print("Para salir escribe salir")
 print("Las operaciones son suma, resta, multi, div")
@@ -8,27 +10,56 @@ valor = 0
 
 n1 = input("Ingresa numero 1: ")
 
+print(n1)
+
+if n1.lower() == "salir":
+    sys.exit()
+
+
 while True:
     while True:
         n2 = input("Ingresa la operacion(suma, resta, multi, div)(salir): ")
-        if n2.lower() == "suma" or "resta" or "multi" or "div" or "salir":
+        if n2.lower() == "suma" or n2.lower() == "resta" or n2.lower() == "multi" or n2.lower() == "div" or n2.lower() == "salir":
             break
 
     if n2.lower() == "salir":
-        break
+        #break
+        sys.exit()
+
     n3 = input("Ingresa numero 2: ")
 
     if n3.lower() == "salir":
-        break
+        #break
+        sys.exit()
 
     if n2.lower() == "suma":
         if bandera:
             resul = int(n1)+int(n3)
             bandera = False
-        elif bandera:
+        else:
             resul = resul + int(n3)
         print("La suma es", resul)
 
     elif n2.lower() == "resta":
-        resul = int(n1)-int(n3)
+        if bandera:
+            resul = int(n1)-int(n3)
+            bandera = False
+        else:
+            resul = resul - int(n3)
         print("La resta es", resul)
+
+    elif n2.lower() == "multi":
+        if bandera:
+            resul = int(n1)*int(n3)
+            bandera = False
+        else:
+            resul = resul * int(n3)
+        print("La multiplicacion es", resul)
+    else:
+        if bandera:
+            resul = int(n1)/int(n3)
+            bandera = False
+        else:
+            resul = resul / int(n3)
+        print("La division es", resul)
+
